f88efa1814f111d0adb0830d7c10fa9b8915f753
LayBug <57842263+LayBug@users.noreply.github.com>
2020-08-29 22:00:23 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mynairalandbot.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
