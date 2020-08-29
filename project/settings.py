f59008f8c675b989ceb5182554cf849d68903523
LayBug <57842263+LayBug@users.noreply.github.com>
2020-08-29 21:49:00 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -21,9 +21,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
