2f1b1955d6067d26d6bb9a79c166c8202383fe04
LayCon <tobitobitobiwhy@gmail.com>
2020-08-29 21:41:48 +0100

    Collected Staticfiles

--- project/settings.py
+++ project/settings.py
@@ -21,9 +21,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mynairalandbot.herokuapp.com',]
 
 
 # Application definition
