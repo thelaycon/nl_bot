8334ea6a5ea9905d4e3063e6860de209b3ca7f17
LayBug <tobitobitobiwhy@gmail.com>
2020-10-19 16:19:12 +0100

    Admin.py changed

--- bot/admin.py
+++ bot/admin.py
@@ -1,1 +1,7 @@
-from django.contrib import admin          from . import models                                                                                                          admin.site.register(models.Profile)
+from django.contrib import admin
+from . import models
+
+
+
+
+admin.site.register(models.Profile)
